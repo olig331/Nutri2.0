57693c3dd2c2f22a71ea742fe8d0233e3842a4ba
olig331 <61230543+olig331@users.noreply.github.com>
2020-09-14 20:47:48 +0100

    minor updates

--- client/src/components/UserSetUp.tsx
+++ client/src/components/UserSetUp.tsx
@@ -1,23 +1,25 @@
-import React, { useContext, useState } from 'react';
+import React, {useRef, useContext, useState, useEffect } from 'react';
 import { SettingsForm } from './SettingsForm';
 import { UsersSettingsContext } from '../Context/Context';
+import PropTypes from 'prop-types';
 
 export const UserSetUp: React.FC = () => {
 
-  const importAll = (r:any) => {
+  const importAll = (r: any) => {
     return r.keys().map(r);
   }
-  //if ts contex error --- npm i -D @types/webpack-env 
-  const images = importAll(require.context('./', false, /\.(png|jpe?g|svg)$/)); 
+  //if you get TS context error --- npm install -D @types/webpack-env 
+  let images = importAll(
+    require.context('../UserIcons', false, /\.(png|jpe?g|svg)$/));
 
   //STATE
   const [nameComplete, setnameComplete] = useState<boolean>(false); // State for Rendering Settings page on compeltion of Username 
   const [uniqueName, setuniqueName] = useState<boolean>(true); // State for when a entered Username in set up is valid(unique) 
+  const [showImgPopUp, setshowImgPopUp] = useState<boolean>(false);
 
   //CONTEXT
   const { userSettings, setuserSettings } = useContext(UsersSettingsContext)
 
-  
   const next = () => {
     setnameComplete(true);
   }
@@ -41,15 +43,52 @@ export const UserSetUp: React.FC = () => {
   const settingName = (e: React.FormEvent<HTMLInputElement>) => {
     setuserSettings({ ...userSettings, [e.currentTarget.name]: e.currentTarget.value });
     setuniqueName(true)
-  }
+  };
 
+  const selectAvatar = (src: string) => {
+    let userSettingsCopy = { ...userSettings }
+    userSettingsCopy.userPicture = src
+    setuserSettings(userSettingsCopy);
+  };
+
+  // const handleClick = (e: MouseEvent) =>{
+  //   if(node.current.contains(e.target as Node)){
+  //     setshowImgPopUp(false);  
+  //   }
+  //   console.log("clicking inside image container")
+  // }
+
+  // useEffect(()=>{
+  //   document.addEventListener("mousedown", handleClick);
+
+  //   return () =>{
+  //     document.removeEventListener("mousedown", handleClick);
+  //   };
+  // },[]);
+
+  // const node = useRef() as React.MutableRefObject<HTMLInputElement>;
+
+  const toggleShowImg = () =>{
+    showImgPopUp ? setshowImgPopUp(false) : setshowImgPopUp(true);
+  }
 
   return (
     <>
       {!nameComplete
         ?
-        (<div>
-          <div style={{ border: "1px solid black", borderRadius: "50%", width: "100px", height: "100px" }}>PIC</div>
+        (<div >
+          <img style={{ width: "75px", height: "75px" }} src={userSettings.userPicture === "" ? images[0] : userSettings.userPicture } alt="User Avatar" />
+          <button onClick={toggleShowImg}>{showImgPopUp?"Close":"Choose Avatar"}</button>
+          {showImgPopUp
+            ?
+            <div>
+              {/* // <div ref={node}> */}
+              {images.map((src: string, index: number) => (
+                <img key={index} onClick={() => selectAvatar(src)} style={{ width: "75px", height: "75px" }} src={src} />
+              ))}
+            </div>
+            : ""
+          }
           <input type="text"
             name="userName"
             onChange={settingName}
